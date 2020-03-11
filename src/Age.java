83e648b4f62c667a032e492e7f763159a1d286e4
DunaevaSvetlana <solnste1993@mail.ru>
2020-03-12 00:52:54 +0400

    Initial commit

---
+++ src/Age.java
@@ -0,0 +1,15 @@
+public class Age {
+    public static void main(String[] args) {
+        final long ageWorld = 13_000_000_000L;
+        final int agePerson = 100_000;
+
+        final long voScolcoRaz = ageWorld / agePerson;
+
+        System.out.println("ageWorld = " + ageWorld);
+        System.out.println("agePerson = " + agePerson);
+        
+        System.out.println("voScolcoRaz = " + voScolcoRaz);
+
+
+    }
+}
